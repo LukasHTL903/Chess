e0b0f00bc1e92fc0fe6e679e757b77f04072b71b
Student <foobar@sr.htlweiz.at>
2026-04-27 09:54:17 +0200

    King_movement_bug

--- Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
+++ Program/obj/Debug/net8.0/Program.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Program")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e154faef1edace6f542937ab9cf171bc2b6924d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c63b39ec186761f8eef6db5b02e5356938d89fa8")]
 [assembly: System.Reflection.AssemblyProductAttribute("Program")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Program")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
